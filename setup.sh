f4a6ad6a5ea42a3114c849d1952900979ea990d6
pobla <a.poblacion@gmail.com>
2013-07-19 02:41:48 +0200

    Update setup.sh
    Added lines for installed nodejs

--- setup.sh
+++ setup.sh
@@ -46,3 +46,13 @@ ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
 ln -sf dotfiles/.emacs.d .
 
+#Install nodejs
+$ sudo apt-get update
+# Install a special package
+$ sudo apt-get install -y python-software-properties python g++ make # Add a new repository for apt-get to search
+$ sudo add-apt-repository ppa:chris-lea/node.js
+# Update apt-get’s knowledge of which packages are where
+$ sudo apt-get update
+# Now install nodejs and npm
+$ sudo apt-get install -y nodejs
+
